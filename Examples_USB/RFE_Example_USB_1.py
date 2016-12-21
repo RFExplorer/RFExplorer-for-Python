ca868a131bc6012703ff537d991bc9c0cc715a49
Ariel Rocholl <arocholl@users.noreply.github.com>
2016-12-21 01:17:06 +0100

    Update RFE_Example_USB_1.py

--- Examples_USB/RFE_Example_USB_1.py
+++ Examples_USB/RFE_Example_USB_1.py
@@ -32,7 +32,7 @@ def PrintPeak(objRFE):
 # global variables and initialization
 #---------------------------------------------------------
 
-SERIALPORT = None    #serial port data 
+SERIALPORT = None    #serial port identifier, use None to autodetect
 BAUDRATE = 500000
 
 objRFE = RFExplorer.RFECommunicator()     #Initialize object and thread
